6cbdb09da5522754b71a48fce6c1f3afcbf4b216
hyunaaah <67725652+hak2711@users.noreply.github.com>
2022-09-12 10:58:13 +0900

    [정렬] 9월 12일 - Update

--- 09월 02일 - 정렬/1758.cpp
+++ 09월 02일 - 정렬/1758.cpp
@@ -5,6 +5,21 @@
 
 using namespace std;
 
+long long calculateTip(vector<int> &arr)
+{
+  long long tip = 0;
+
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if (arr[i] <= i)
+    {
+      return tip;
+    }
+    tip += (arr[i] - i);
+  }
+  return tip;
+}
+
 int main()
 {
 
@@ -20,15 +35,5 @@ int main()
 
   sort(arr.begin(), arr.end(), greater<>());
 
-  long long tip = 0;
-
-  for (int i = 0; i < n; i++)
-  {
-    if (arr[i] > i)
-    {
-      tip += (arr[i] - i);
-    }
-  }
-
-  cout << tip;
+  cout << calculateTip(arr);
 }
