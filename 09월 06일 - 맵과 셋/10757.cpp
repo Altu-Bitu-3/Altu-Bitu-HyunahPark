a310ae6e4d8b5d822e2a32690a72bf5ffe66e71e
hyunaaah <67725652+hak2711@users.noreply.github.com>
2022-09-13 10:50:51 +0900

    [맵과 셋] 09월 13일 - Update

--- 09월 06일 - 맵과 셋/10757.cpp
+++ 09월 06일 - 맵과 셋/10757.cpp
@@ -7,11 +7,9 @@ string insertNumtoString(string &s, int num)
   return s.insert(0, 1, (char)(num + '0'));
 }
 
-int main()
+string addTwoNums(string &s1, string &s2)
 {
-  string s1, s2, answer;
-
-  cin >> s1 >> s2;
+  string answer;
 
   int i = s1.size() - 1, j = s2.size() - 1;
   int carry = 0, next;
@@ -43,5 +41,14 @@ int main()
     insertNumtoString(answer, carry);
   }
 
-  cout << answer;
+  return answer;
+}
+
+int main()
+{
+  string s1, s2;
+
+  cin >> s1 >> s2;
+
+  cout << addTwoNums(s1, s2);
 }
