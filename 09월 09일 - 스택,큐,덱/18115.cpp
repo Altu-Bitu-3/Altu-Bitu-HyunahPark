407ccc65a549c9d13d7215903ea88c9115da0934
hyunaaah <67725652+hak2711@users.noreply.github.com>
2022-09-17 14:41:30 +0900

    [스택,큐,덱] 09월 17일 - Update

--- 09월 09일 - 스택,큐,덱/18115.cpp
+++ 09월 09일 - 스택,큐,덱/18115.cpp
@@ -4,30 +4,30 @@
 
 using namespace std;
 
-deque<int> calculate(deque<int> &d, vector<int> cal, int n)
+deque<int> calculate(vector<int> &cal, int n)
 {
   deque<int> original;
+  int num = 1;
 
   for (int i = 0; i < n; i++)
   {
     switch (cal[i])
     {
     case 1:
-      original.push_front(d.back());
+      original.push_front(num++);
       break;
     case 2:
     {
       int tmp = original.front();
       original.pop_front();
-      original.push_front(d.back());
+      original.push_front(num++);
       original.push_front(tmp);
       break;
     }
     case 3:
-      original.push_back(d.back());
+      original.push_back(num++);
       break;
     }
-    d.pop_back();
   }
 
   return original;
@@ -40,18 +40,12 @@ int main()
 
   vector<int> cal(n);
 
-  for (int i = 1; i <= n; i++)
+  for (int i = n-1; i >= 0; i--)
   {
-    cin >> cal[n - i];
+    cin >> cal[i];
   }
 
-  deque<int> d;
-  for (int i = 1; i <= n; i++)
-  {
-    d.push_front(i);
-  }
-
-  deque<int> result = calculate(d, cal, n);
+  deque<int> result = calculate(cal, n);
 
   for (int i : result)
   {
